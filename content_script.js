ace542a2e8608593eda071cb969410980ea9f375
unknown <Jason@.(none)>
2012-03-02 09:44:06 -0700

    checked the code against a google apps account. One code change is needed in content_script.js if you switch between the two.

--- content_script.js
+++ content_script.js
@@ -9,7 +9,12 @@ if (window == top) {
  
  
  var ifr = $('[name="to"]', $canvasframe);
- var gmailquote = $($canvasframe.find('iframe')[1].contentDocument).find('div.gmail_quote')[0];
+ 
+ //use this for standard gmail (1):
+ //var gmailquote = $($canvasframe.find('iframe')[1].contentDocument).find('div.gmail_quote')[0];
+ 
+ //use this for google apps gmail (0)
+ var gmailquote = $($canvasframe.find('iframe')[0].contentDocument).find('div.gmail_quote')[0];
  
  sendResponse(ifr.parent().children()[1].innerHTML + "~" + gmailquote.innerHTML);
  //document.getElementsByName("to")[0].nextSibling.innerHTML
